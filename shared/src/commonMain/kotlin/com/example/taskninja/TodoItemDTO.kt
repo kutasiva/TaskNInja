11e013a8b8773831b74513c7c6b24415d98f7715
kutasiva <kutasov@seznam.cz>
2025-03-14 13:19:25 +0100

    Add Serializable to DTO model

--- shared/src/commonMain/kotlin/com/example/taskninja/TodoItemDTO.kt
+++ shared/src/commonMain/kotlin/com/example/taskninja/TodoItemDTO.kt
@@ -1,7 +1,9 @@
 package com.example.taskninja
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class TodoItemDTO(
     val id: Int,
     val userId: Int,
